c6824b9f9e2f04527ebb5a290a70d7c772dc0b39
jagrutipatil0106 <jagrutipatil0106@gmail.com>
2020-03-04 21:53:15 +0530

    address of datatypes

---
+++ PractiseProblem01/AddressOfDatatypes.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int *i;
+
+	int *d;
+
+cout<<"address of i is: "<<i<<endl;
+cout<<"address of d is: "<<d<<endl;
+
+i = d;
+
+cout<<i<<endl;
+
+
+
+	return 0;
+}
---
+++ PractiseProblem01/AddressOfDiffDataTypes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ using namespace std;
+
+ int main(){
+ 	
+int *i;
+float *f;
+
+ cout<<"Address of i :"<<i<<endl;
+ cout<<"address of f is: "<<f<<endl ;
+
+i = f;
+
+cout<<i<<endl;
+
+
+ 	return 0;
+
+
+ }
