11efcc60081b7f15ed8427726dc6e91eddb949b8
jagrutipatil0106 <jagrutipatil0106@gmail.com>
2020-03-24 02:18:50 +0530

    it is not showing any output

--- PractisePrograms/P01.cpp
+++ PractisePrograms/P01.cpp
@@ -1,45 +1,32 @@
-//ProblemStatement:
-//print an array in descending order.
+//integers, a and b (obtained by the user) and will perform the division a/b,
+//store the result in another integer c and show the result of the division using cout. 
+//In a similar way, extend the program to add, subtract, multiply, do modulo and power using integers a and b. 
+//Modify your program so that when it starts, it asks the user which type of calculation it should do, then asks 
+//for the 2 integers, then runs the user selected calculation and outputs the result in a user friendly formatted manner. 
+	//solution #1
 
 #include<iostream>
 using namespace std;
 int main()
 {
-	int N;
-	int temp;
-
-	cout<<"Enter the count of an array : "<<endl;
-	cin>>N;
-
-
+	int N ;
 	int arr[N];
+	int b = 0;
+	cout<<"jsdfsdj";
+
+	cout<<"Enter the array count :";
+	cin>>N;
 
 	for (int i = 0; i < N; ++i)
 	{
 		cin>>arr[i];
-	}
-	for (int i = 0; i < N; ++i)
-	{
-		cout<<arr[i]<<" ";
-	}
-	for (int i = 0; i < N; ++i)
-	{
-		for(int j = i+1; j < N; ++j)
+		if (arr[i]>=10)
 		{
-		if (arr[i] < arr[j]) //here j is i+1 means it is at 1th position
-			                 //and i is at 0 th position so they can 
-			                 //compare themselves.
-		{
-			temp = arr[i];
-			arr[i] = arr[j];
-			arr[j] =temp;
+			b++;
 		}
-	    }
-	    cout<<endl;
-	    cout<<arr[i]<<" ";
 	}
+	cout<<"The number of integers greater than 10 is : "<<b;
 
-	
 
 	return 0;
 }
